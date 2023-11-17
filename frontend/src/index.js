11f0ef1bdc1c2c7b7a3be0634e311042c0c12da6
CASTVELL <lmcastellanosa@correo.udistrital.edu.co>
2023-11-17 01:23:54 -0500

    Update .gitignore, Dockerfile, and added necesary files for the correct frontend config

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FilterContainer from './containers/FilterContainer';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+
+
+    <FilterContainer />
+
+  </React.StrictMode>
+);
+
